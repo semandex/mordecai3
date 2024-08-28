c20e2b1f270733de496322d3b213682c230a80a0
Chirag Patel <chirag@semandex.net>
2024-08-28 17:16:13 -0400

    #2 Update the version as per the review suggestions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='mordecai3',
-    version='3.0.0.0',
+    version='3.0.0a.0',
     url='http://github.com/ahalterman/mordecai3/',
     author='Andy Halterman',
     author_email='ahalterman0@gmail.com',
